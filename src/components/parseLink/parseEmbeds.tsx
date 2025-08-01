b3f48cdf00fa3e77c68fcb066e4831d322c04389
innocent <terminalkarabo@gmail.com>
2025-08-02 00:31:56 +0200

    feature: add html-react-parser and sanitize-html dependencies; implement TweetCard component for Twitter embeds

--- src/components/parseLink/parseEmbeds.tsx
+++ src/components/parseLink/parseEmbeds.tsx
@@ -1,34 +1,50 @@
 export function parseEmbeds(html: string): string {
   // YouTube
-  const youtubeRegex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]{11})/g;
-  html = html.replace(youtubeRegex, (match, videoId) => {
+  const youtubeRegex =
+    /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]{11})/g;
+  html = html.replace(youtubeRegex, (_match, videoId) => {
     return `
-      <div class="my-6 w-full aspect-video">
-        <iframe
-          src="https://www.youtube.com/embed/${videoId}"
-          frameborder="0"
-          allowfullscreen
-          class="w-full h-full rounded-lg"
-        ></iframe>
-      </div>
+      <iframe
+        src="https://www.youtube.com/embed/${videoId}"
+        frameborder="0"
+        allowfullscreen
+        class="w-full h-full rounded-lg"
+      ></iframe>
     `;
   });
 
   // Vimeo
   const vimeoRegex = /(?:https?:\/\/)?(?:www\.)?vimeo\.com\/(\d+)/g;
-  html = html.replace(vimeoRegex, (match, videoId) => {
+  html = html.replace(vimeoRegex, (_match, videoId) => {
     return `
-      <div class="my-6 aspect-w-16 aspect-h-9">
-        <iframe
-          src="https://player.vimeo.com/video/${videoId}"
-          frameborder="0"
-          allow="autoplay; fullscreen; picture-in-picture"
-          allowfullscreen
-          class="w-full h-full"
-        ></iframe>
-      </div>
+      <iframe
+        src="https://player.vimeo.com/video/${videoId}"
+        frameborder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowfullscreen
+        class="w-full h-full"
+      ></iframe>
     `;
   });
 
+  // Twitter/X
+  const twitterRegex =
+    /(?:https?:\/\/)?(?:www\.)?(?:twitter\.com|x\.com)\/[^/]+\/status\/(\d+)/g;
+  html = html.replace(twitterRegex, (_match, tweetId) => {
+    return `<tweet-embed data-id="${tweetId}"></tweet-embed>`;
+  });
+
+  // This is the crucial fix. We find all <p> tags and inspect their content.
+  html = html.replace(/<p>([\s\S]*?)<\/p>/g, (match, content) => {
+    if (content.includes('<iframe') || content.includes('<tweet-embed')) {
+      return `<div>${content}</div>`;
+    }
+    
+
+    
+    // Otherwise, if it's just a normal paragraph, we leave it as is.
+    return match;
+  });
+  
   return html;
 }
